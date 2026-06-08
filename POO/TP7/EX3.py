3fee6c9d9aa476b8677d6479bdc85b3bdf30e6d7
Achraf <achraf.arajoum@gmail.com>
2026-06-08 11:40:23 +0100

    Update EX3.py

--- POO/TP7/EX3.py
+++ POO/TP7/EX3.py
@@ -1,13 +1,13 @@
 """
 liaisons = [
-(&quot;A&quot;,&quot;B&quot;),
-(&quot;A&quot;,&quot;C&quot;),
-(&quot;B&quot;,&quot;D&quot;),
-(&quot;C&quot;,&quot;D&quot;),
-(&quot;D&quot;,&quot;E&quot;)
+("A","B"),
+("A","C"),
+("B","D"),
+("C","D"),
+("D","E")
 ]
 1. Construire le graphe avec defaultdict(list).
-2. Afficher tous les voisins d&#39;un sommet.
+2. Afficher tous les voisins d'un sommet.
 3. Déterminer si un chemin existe entre A et E.
 """
 
@@ -23,27 +23,11 @@ from collections import defaultdict
 
 # 1. Construire le graphe avec defaultdict(list).
 graph = defaultdict(list)
-for u, v in liaisons:
-    graph[u].append(v)
-    graph[v].append(u)  
+for i, j in liaisons:
+    graph[i].append(j)
+    graph[j].append(i)
+print(graph)
 
-# 2. Afficher tous les voisins d'un sommet.
-def afficher_voisins(sommet):
-    voisins = graph[sommet]
-    print(f"Les voisins de {sommet} sont: {', '.join(voisins)}")
-
-# 3. Déterminer si un chemin existe entre A et E.
-def existe_chemin(graph, start, end, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(start)
-    
-    if start == end:
-        return True
-    
-    for neighbor in graph[start]:
-        if neighbor not in visited:
-            if existe_chemin(graph, neighbor, end, visited):
-                return True
-    return False
+#2. Afficher tous les voisins d'un sommet.
+print(graph["A"])
 
